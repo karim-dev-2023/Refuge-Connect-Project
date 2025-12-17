24d627394422591b6b04e46fdf9df8af019b43bb
Karim <karim.ayoub.job@gmail.com>
2025-12-17 14:55:10 +0100

    Add show, update and delete animal function in controller

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -16,8 +16,42 @@ class AnimalController extends Controller
             'age' => 3,
             'description' => 'Une Shiba Inu vive et joyeuse, toujours prête à jouer et à explorer. Coquillette est curieuse, intelligente et très expressive, avec un petit caractère indépendant mais plein de tendresse pour ceux qu’elle aime.',
             'photo' => 'images/animaux/coquillette.png',
-        ]); 
+        ]);
 
         return "Ajout éffectué";
+    }
+
+    public function showAnimal($id) {
+        // Récupérer un enregistrement par son identifiant
+        $item = Animals::find($id);
+
+        if (!$item) {
+            return "Animal non trouvé";
+        }
+        return view('welcome', ["article" => $item]);
+    }
+
+    public function updateAnimal($id)
+    {
+        $article = Animals::findOrFail($id);
+
+        $article->update([
+            'name' => $article->name . ' modifié'
+        ]);
+
+        return 'Mise à jour effectuée';
+    }
+
+    public function deleteAnimal($id)
+    {
+        $article = Animals::find($id);
+
+        if (!$article) {
+            return 'Animal non trouvé';
+        }
+
+        $article->delete();
+
+        return 'Suppression effectuée';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',  [IndexController::class, 'index'])->name('home');
 
 Route::prefix('animal')->group(function () {
-    Route::get('/create', [AnimalController::class, 'addAnimal'])->name('article.ajoutAnimal');
-    // Route::get('/{id}', [AnimalController::class, 'show'])->name('article.details');
-    // Route::get('/update/{id}', [AnimalController::class, 'updateItem'])->name('article.misAJourArticle');
-    // Route::get('/delete/{id}', [AnimalController::class, 'deleteItem'])->name('article.suppresionArticle');
+    Route::get('/create', [AnimalController::class, 'addAnimal'])->name('animal.ajoutAnimal');
+    Route::get('/{id}', [AnimalController::class, 'showAnimal'])->name('animal.details');
+    Route::get('/update/{id}', [AnimalController::class, 'updateAnimal'])->name('animal.misAJourAnimal');
+    Route::get('/delete/{id}', [AnimalController::class, 'deleteAnimal'])->name('animal.suppresionAnimal');
 });
