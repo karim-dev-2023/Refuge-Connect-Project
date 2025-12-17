32c1c47919cd4fa6351838884d6dd21d4be6ffe7
Karim <karim.ayoub.job@gmail.com>
2025-12-17 14:45:41 +0100

    Creation partials and layouts blade files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="{{ Storage::url("images/favicon.png") }}">
+</head>
+
+<body>
+    @include('partials.header')
+    <main>
+        @yield('content')
+    </main>
+    @include('partials.footer')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-</head>
-
-<body>
+<!-- resources/views/welcome.blade.php -->
+@extends('layouts.app')
+@section('title', 'Accueil')
+@section('content')
+    
    
-</body>
 
-</html>
+@endsection
